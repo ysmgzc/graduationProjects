c828b93c185da20839902cccd3db38eccef46322
tugranDemirel <demireltugran66@gmail.com>
2021-10-20 23:01:37 +0300

    bildirimler için younes toastr kurulumu

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // toastr provider
+        Yoeunes\Toastr\ToastrServiceProvider::class,
+
     ],
 
     /*
