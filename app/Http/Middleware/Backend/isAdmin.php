857df18920490a0a1cfe828f39dab2693fb456c9
tugranDemirel <demireltugran66@gmail.com>
2021-10-30 23:43:20 +0300

    admin login olduğu zaman giriş sayfasına gidememe kontrolu, admin login olmadan admin paneline gidemem kontrolleri yapıldı

--- app/Http/Middleware/Backend/isAdmin.php
+++ app/Http/Middleware/Backend/isAdmin.php
@@ -17,7 +17,7 @@ class isAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!Auth::check() && Auth::user()->statu == 1)
+        if (!Auth::check())
             return redirect()->route('login');
         return $next($request);
     }
