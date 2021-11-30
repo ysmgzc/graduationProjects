f863db1d778e3607f6abb310c1e1d999fbdf6bb6
tugranDemirel <demireltugran66@gmail.com>
2021-11-30 21:06:08 +0300

    ürünlere ait tabloların oluşturulması ve düzenlenmesi

---
+++ database/migrations/2021_11_30_180102_create_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('description');
+            $table->string('keywords');
+            $table->string('hash');
+            $table->float('price');
+            $table->float('discount')->default(0);
+            $table->unsignedBigInteger('menu_id');
+            $table->unsignedBigInteger('brand_id');
+            $table->foreign('menu_id')->references('id')->on('menu');
+            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->longText('images');
+            $table->string('code');
+            $table->integer('status');
+            $table->integer('hit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
