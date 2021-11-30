752c8b62375c7b2a797765174860d560eb398104
tugranDemirel <demireltugran66@gmail.com>
2021-11-30 21:40:40 +0300

    gereksiz kısımların kaldırılması/yorum satırına alınması

--- resources/views/Backend/layouts/sidebar.blade.php
+++ resources/views/Backend/layouts/sidebar.blade.php
@@ -38,6 +38,7 @@
                     </li>
                 </ul>
             </li>
+            <!--
             <li><a class="sidebar-header" href="#"><i data-feather="dollar-sign"></i><span>Satış</span><i class="fa fa-angle-right pull-right"></i></a>
                 <ul class="sidebar-submenu">
                     <li><a href="order.html"><i class="fa fa-circle"></i>Siparişler</a></li>
@@ -50,13 +51,13 @@
                     <li><a href="coupon-create.html"><i class="fa fa-circle"></i>Kupon Oluştur </a></li>
                 </ul>
             </li>
+            -->
             <li><a class="sidebar-header" href="#"><i data-feather="clipboard"></i><span>Sayfalar</span><i class="fa fa-angle-right pull-right"></i></a>
                 <ul class="sidebar-submenu">
                     <li><a href="{{route('sozlesme.index')}}"><i class="fa fa-circle"></i>Sayfa Listesi</a></li>
                     <li><a href="{{route('sozlesme.create')}}"><i class="fa fa-circle"></i>Sayfa Oluştur</a></li>
                 </ul>
             </li>
-            <li><a class="sidebar-header" href="media.html"><i data-feather="camera"></i><span>Medya</span></a></li>
             <li><a class="sidebar-header" href="#"><i data-feather="align-left"></i><span>Menü</span><i class="fa fa-angle-right pull-right"></i></a>
                 <ul class="sidebar-submenu">
                     <li><a href="{{route('menu.index')}}"><i class="fa fa-circle"></i>Menü Listesi</a></li>
@@ -65,6 +66,7 @@
                     <li><a href="{{route('submenu.create')}}"><i class="fa fa-circle"></i>Alt Menü Oluştur</a></li>
                 </ul>
             </li>
+            <!--
             <li><a class="sidebar-header" href="#"><i data-feather="user-plus"></i><span>Kullanıcılar</span><i class="fa fa-angle-right pull-right"></i></a>
                 <ul class="sidebar-submenu">
                     <li><a href="user-list.html"><i class="fa fa-circle"></i>Kullanıcı Listesi</a></li>
@@ -83,7 +85,8 @@
                     <li><a href="taxes.html"><i class="fa fa-circle"></i>Vergiler</a></li>
                 </ul>
             </li>
-            <li><a class="sidebar-header" href="reports.html"><i data-feather="bar-chart"></i><span>Satış Raporu</span></a></li>
+
+            <li><a class="sidebar-header" href="reports.html"><i data-feather="bar-chart"></i><span>Satış Raporu</span></a></li> -->
             <li><a class="sidebar-header" href="#"><i data-feather="settings" ></i><span>Ayarlar</span><i class="fa fa-angle-right pull-right"></i></a>
                 <ul class="sidebar-submenu">
                     <li><a href="{{route('admin.profil')}}"><i class="fa fa-circle"></i>Profil</a></li>
@@ -92,8 +95,10 @@
                     <li><a href="{{route('admin.contact.settings')}}"><i class="fa fa-circle"></i>İletişim Ayarları</a></li>
                 </ul>
             </li>
+            <!--
             <li><a class="sidebar-header" href="invoice.html"><i data-feather="archive"></i><span>Kesilen Faturalar</span></a>
             </li>
+            -->
             <li><a class="sidebar-header" href="{{route('logout')}}"><i data-feather="log-in"></i><span>Çıkış Yap</span></a>
             </li>
         </ul>
